894235b78a14c2d279efff91a9075719c2974274
Oskar Kaczmarski <oskar2.kaczmarski@live.uwe.ac.uk>
2024-01-29 18:47:01 +0000

    starting visual representation of UI
     - finished backend
     - this tutorial is nuts

---
+++ Assets/GEP/Classes/Inventory/UIScript/InventoryDisplay.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public abstract class InventoryDisplay : MonoBehaviour
+{
+    [SerializeField] MouseItemData mouseInventoryItem;
+
+    protected InventorySystem inventorySystem;
+    protected Dictionary<InventorySlot_UI, InventorySlot> slotDictionary;
+    public InventorySystem InventorySystem => inventorySystem;
+    public Dictionary<InventorySlot_UI, InventorySlot> SlotDictionary => slotDictionary;
+
+    protected virtual void Start()
+    {
+
+    }
+
+    public abstract void AssignSlot(InventorySystem invToDisplay);
+
+    protected virtual void UpdateSlot(InventorySlot updatedSlot)
+    {
+        foreach (var slot in SlotDictionary)
+        {
+            if (slot.Value == updatedSlot) // slot value - inventory slot
+            {
+                slot.Key.UpdateUISlot(updatedSlot); // the ui representation of slot
+            }
+        }
+    }
+
+    public void SlotClicked(InventorySlot_UI clickedSlot)
+    {
+        Debug.Log("Slot clicked");
+    }
+}
